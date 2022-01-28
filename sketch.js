9f50102732d95d61dc5453409a8f0ef2463b1d4f
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-28 17:45:28 +0100

    use map class instead

--- sketch.js
+++ sketch.js
@@ -2,20 +2,24 @@ let pointList = [];
 let connectedPoints = [];
 let ms = 0;
 
-let points = 100;
+let map;
+
+let points = 10;
 let pointConnections = 3; //can be at most points-1
 
 function setup() {
   createCanvas(800, 800);
   background(150);
 
-  for (var i = 0; i < points; i++) {
+  map = new Map(points);
+
+  /*for (var i = 0; i < points; i++) {
     pointList[i] = createVector(floor(random(width)), floor(random(height))); //places points at random
     connectedPoints[i] = [];
   }
   //randomConnections();
   closestConnections();
-
+*/
   textSize(12);
 }
 
@@ -24,14 +28,15 @@ function draw() {
   fill(255);
 
   background(150);
+  map.displayPoints();
   //draw points and their connections
-  for (var i = 0; i < pointList.length; i++) {
+  /*for (var i = 0; i < pointList.length; i++) {
     //circle(pointList[i].x, pointList[i].y, 10);
     text(i,pointList[i].x, pointList[i].y);
     for (var j = 0; j < pointConnections; j++) {
       line(pointList[i].x, pointList[i].y, pointList[connectedPoints[i][j]].x, pointList[connectedPoints[i][j]].y);
     }
-  }
+  }*/
 
   fill(255, 0, 0);
   text(floor((millis() - ms) * 1000), 10, 10);
