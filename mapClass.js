c94192b1be04e7a2351588a50d2f41952dc386ef
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-28 17:39:40 +0100

    added functions
    + createRandomPoints()
    + addPoint()
    + displayPoints()

--- mapClass.js
+++ mapClass.js
@@ -1,5 +1,27 @@
 class Map {
-  constructor() {
+  constructor(_points) {
+    this.points =_points;
+    this.pointList = [];
 
+  }
+
+  createRandomPoints(_points){
+    this.points = _points;
+    for (var i = 0; i < this.points; i++) {
+      this.pointList[i] = createVector(floor(random(width)), floor(random(height))); //places points at random
+    }
+  }
+
+  addPoint(_x, _y){
+    this.pointList.push(createVector(_x, _y));
+    this.points +=1;
+  }
+
+  displayPoints(){
+    for (var i = 0; i < this.points; i++) {
+      //circle(pointList[i].x, pointList[i].y, 10);
+      text(i,this.pointList[i].x, this.pointList[i].y);
+
+    }
   }
 }
