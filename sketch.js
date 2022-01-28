0aa7707ef5b05071ed54657544fd1eb16d9002da
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-28 20:28:19 +0100

    cleaning

--- sketch.js
+++ sketch.js
@@ -1,11 +1,6 @@
-let pointList = [];
-let connectedPoints = [];
 let ms = 0;
 
 let map;
-
-let points = 10;
-let pointConnections = 3; //can be at most points-1
 
 function setup() {
   createCanvas(800, 800);
@@ -21,13 +16,6 @@ function setup() {
   map.addPoint(400, 500);
   map.addPoint(700, 700);
 
-  /*for (var i = 0; i < points; i++) {
-    pointList[i] = createVector(floor(random(width)), floor(random(height))); //places points at random
-    connectedPoints[i] = [];
-  }
-  //randomConnections();
-  closestConnections();
-*/
   textSize(12);
 }
 
@@ -38,14 +26,6 @@ function draw() {
   background(150);
   map.displayPoints();
   map.displayLines();
-  //draw points and their connections
-  /*for (var i = 0; i < pointList.length; i++) {
-    //circle(pointList[i].x, pointList[i].y, 10);
-    text(i,pointList[i].x, pointList[i].y);
-    for (var j = 0; j < pointConnections; j++) {
-      line(pointList[i].x, pointList[i].y, pointList[connectedPoints[i][j]].x, pointList[connectedPoints[i][j]].y);
-    }
-  }*/
 
   fill(255, 0, 0);
   text(floor((millis() - ms) * 1000), 10, 10);
