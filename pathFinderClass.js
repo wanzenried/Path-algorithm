8ca8d394a0dc10a829d24ff039c4fdb91955b5dc
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-28 21:15:04 +0100

    added loading map, and setting route

--- pathFinderClass.js
+++ pathFinderClass.js
@@ -2,4 +2,18 @@ class PathFinder {
   constructor() {
 
   }
+
+  loadMap(_map){
+    this.map = _map;
+    this.points = _map.pointList.length;
+  }
+
+  setRoute(_start, _finish){
+    if((_start > -1 && _start < this.points) && (_finish > -1 && _finish < this.points)){
+      this.start = _start;
+      this.finish = _finish;
+      this.shortestDist = distTwoPoints(this.map.pointList[_start].x, this.map.pointList[_start].y, this.map.pointList[_finish].x, this.map.pointList[_finish].y);
+    }
+
+  }
 }
