f7e1d99bdec4f55bca65f3e78730973cc682a93f
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-28 20:59:08 +0100

    added static start map

--- sketch.js
+++ sketch.js
@@ -8,13 +8,32 @@ function setup() {
 
   map = new Map();
   map.addPoint(200, 200);
-  map.addPoint(300, 300);
+  map.addPoint(250, 300);
   map.addPoint(400, 100);
   map.addPoint(500, 200);
-  map.addPoint(100, 300);
-  map.addPoint(200, 400);
-  map.addPoint(400, 500);
+  map.addPoint(100, 250);
+  map.addPoint(570, 400);
+  map.addPoint(400, 370);
   map.addPoint(700, 700);
+  map.addPoint(500, 530);
+  map.addPoint(300, 510);
+
+  map.addConnection(4,0);
+  map.addConnection(0,1);
+  map.addConnection(1,2);
+  map.addConnection(2,3);
+  map.addConnection(1,6);
+  map.addConnection(3,5);
+  map.addConnection(6,3);
+  map.addConnection(6,5);
+  map.addConnection(5,8);
+  map.addConnection(8,7);
+  map.addConnection(7,5);
+  map.addConnection(8,9);
+  map.addConnection(9,1);
+  map.addConnection(9,6);
+  map.addConnection(4,9);
+
 
   textSize(12);
 }
