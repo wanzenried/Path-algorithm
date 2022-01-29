34b6a7a2c1c869fa435f9a9235c01243f1750de2
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-29 11:25:56 +0100

    added faster distance function

--- EOUFunctions.js
+++ EOUFunctions.js
@@ -1,6 +1,13 @@
-function distTwoPoints(_p1, _p2) {
+function distTwoPointsAbs(_p1, _p2) {
   let xDif = _p1.x - _p2.x;
   let yDif = _p1.y - _p2.y;
   let dist = sqrt((xDif * xDif) + (yDif * yDif));
+  return dist;
+}
+
+function distTwoPointsRel(_p1, _p2){
+  let xDif = _p1.x - _p2.x;
+  let yDif = _p1.y - _p2.y;
+  let dist = (xDif * xDif) + (yDif * yDif);
   return dist;
 }
