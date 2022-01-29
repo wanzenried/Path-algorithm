3aeba769138b6aed573a45b57c565b901c09e1d2
Richard <62063187+wanzenried@users.noreply.github.com>
2022-01-29 11:14:56 +0100

    fixed an error

--- pathFinderClass.js
+++ pathFinderClass.js
@@ -12,7 +12,7 @@ class PathFinder {
     if((_start > -1 && _start < this.points) && (_finish > -1 && _finish < this.points)){
       this.start = _start;
       this.finish = _finish;
-      this.shortestDist = distTwoPoints(this.map.pointList[_start].x, this.map.pointList[_start].y, this.map.pointList[_finish].x, this.map.pointList[_finish].y);
+      this.shortestDist = distTwoPoints(this.map.pointList[_start], this.map.pointList[_finish]);
     }
   }
 
