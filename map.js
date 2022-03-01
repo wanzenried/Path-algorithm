3284d451476dde085e255cb8e20716fbc30666af
Richard <62063187+wanzenried@users.noreply.github.com>
2022-03-01 19:52:47 +0100

    check if new point is inside map

--- map.js
+++ map.js
@@ -1,5 +1,9 @@
 class Map {
   constructor(mapWidth, mapHeight) {
+    this.mapWidth = mapWidth;
+    this.mapHeight = mapHeight;
+    //create list to contain points
+    this.points = [];
     //create the map, with no points
     this.map = [];
     for (var i = 0; i < mapWidth; i++) {
@@ -7,6 +11,13 @@ class Map {
       for (var j = 0; j < mapHeight; j++) {
         this.map[i][j] = -1;
       }
+    }
+  }
+
+  addPoint(pointX, pointY){
+    //first check if the point is inside the map
+    if(!((pointX >= 0 && pointX < this.mapWidth) && (pointY >= 0 && pointY < this.mapHeight))){
+      throw "point is outside map";
     }
   }
 }
