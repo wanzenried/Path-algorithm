b2bed41c45342c1171ad6ddee4509b6343ee8f01
Richard <62063187+wanzenried@users.noreply.github.com>
2022-03-01 17:49:52 +0100

    lets start over

--- EOUFunctions.js
+++
@@ -1,6 +0,0 @@
-function distTwoPoints(_p1, _p2) {
-  let xDif = _p1.x - _p2.x;
-  let yDif = _p1.y - _p2.y;
-  let dist = sqrt((xDif * xDif) + (yDif * yDif));
-  return dist;
-}
--- sketch.js
+++ sketch.js
@@ -1,32 +1,7 @@
-let ms = 0;
-
-let map;
-
 function setup() {
-  createCanvas(800, 800);
-  background(150);
 
-  map = new Map();
-  map.addPoint(200, 200);
-  map.addPoint(300, 300);
-  map.addPoint(400, 100);
-  map.addPoint(500, 200);
-  map.addPoint(100, 300);
-  map.addPoint(200, 400);
-  map.addPoint(400, 500);
-  map.addPoint(700, 700);
-
-  textSize(12);
 }
 
 function draw() {
-  ms = millis();
-  fill(255);
 
-  background(150);
-  map.displayPoints();
-  map.displayLines();
-
-  fill(255, 0, 0);
-  text(floor((millis() - ms) * 1000), 10, 10);
 }
