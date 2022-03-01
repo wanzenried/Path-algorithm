7e9cd17a7ceadf92fddb0192f38ad27e0669b25d
Richard <62063187+wanzenried@users.noreply.github.com>
2022-03-01 20:23:28 +0100

    draw all points

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
+let m;
+
 function setup() {
   createCanvas(800,800);
   background(230);
+
+  m = new Map(width, height);
+
   noLoop();
 
 }
 
 function draw() {
+
+  for (var i = 0; i < m.points.length; i++) {
+    circle(m.points[i].x  ,m.points[i].y,5);
+  }
 
 }
