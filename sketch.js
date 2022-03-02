8809a81956e052f9c4af1d16fc35413970abcc61
Richard <62063187+wanzenried@users.noreply.github.com>
2022-03-02 21:31:18 +0100

    cosmetic

--- sketch.js
+++ sketch.js
@@ -7,12 +7,14 @@ function setup() {
   m = new Map(width, height);
   m.addRandomPoint(2);
 
+  noStroke();
   noLoop();
 
 }
 
 function draw() {
-  background(230);
+  background(110);
+  fill(255);
 
   for (var i = 0; i < m.points.length; i++) {
     circle(m.points[i].x  ,m.points[i].y,5);
